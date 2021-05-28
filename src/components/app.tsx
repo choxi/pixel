7aaafd74be863ad12ad6da0c0ca5364fcfd838f2
Roshan Choxi <roshan.choxi@gmail.com>
2021-05-28 15:04:30 -0500

    Installs AceEditor

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,17 @@
 import React from "react"
+import AceEditor from "react-ace"
+
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-github";
 
 export default class App extends React.Component {
   render() {
     return <div>
-      <h1>App</h1>
+      <AceEditor
+        mode="javascript"
+        theme="github"
+        name="ace"
+        editorProps={{ $blockScrolling: true }} />,
     </div>
   }
 }
