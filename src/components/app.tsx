c31f121f69817b8cc5ac3aa9cb2b272766101ff1
Roshan Choxi <roshan.choxi@gmail.com>
2021-05-30 14:51:40 -0500

    Adds ace-build, some timeline styles

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import React from "react"
 import AceEditor from "react-ace"
 
@@ -12,6 +14,7 @@ interface Props {
 
 interface State {
   code: String
+  images: [any]
 }
 
 const defaultCode = `
@@ -22,12 +25,29 @@ p.setup = () => {
 `
 
 export default class App extends React.Component<Props, State> {
-  state = { code: defaultCode }
+  state = { code: defaultCode, images: [] }
   previewRef: React.RefObject<HTMLDivElement> = React.createRef()
+  framesRef: React.RefObject<HTMLDivElement> = React.createRef()
   p: p5 = new p5(() => {})
+  images: [any] = []
 
   componentDidMount() {
+    p5.prototype.registerMethod("post", () => this.handleDraw())
     this.updatePreview()
+  }
+
+  handleDraw() {
+    const canvas = this.previewRef.current.querySelector("canvas")
+    // this.images.push(canvas.toDataURL())
+    this.state.images.push(canvas.toDataURL())
+    this.setState({ images: this.state.images }, () => {
+
+      this.framesRef.current.scrollBy(this.framesRef.current.scrollWidth, 0)
+    })
+  }
+
+  saveTimeline() {
+    this.setState({ images: this.images }, () => this.images = [])
   }
 
   handleChange(code: String) {
@@ -48,21 +68,40 @@ export default class App extends React.Component<Props, State> {
   }
 
   render() {
-    return <div className="layout-split">
-      <div className="layout-split-halfpanel">
-        <AceEditor
-          name="ace"
-          theme="github"
-          mode="javascript"
-          defaultValue={ this.state.code }
-          width="100%"
-          height="100%"
-          showPrintMargin={ false }
-          onChange={ code => this.handleChange(code) }
-          editorProps={{ $blockScrolling: true }} />
+    // We need to set useWorker=false to fix the `Failed to
+    // execute 'importScripts' on 'WorkerGlobalScope'` error
+    // https://github.com/securingsincity/react-ace/issues/725
+    const timelineFrames = this.state.images.map((image, index) => {
+      return <div key={ index } className="timeline-frame">
+        <img src={ image } />
       </div>
-      <div className="layout-split-halfpanel">
-        <div className="preview" style={{ height: "100%" }} ref={ this.previewRef }> </div>
+    })
+
+    return <div className="layout-vstack">
+      <div className="layout-split">
+        <div className="layout-split-halfpanel">
+          <AceEditor
+            name="ace"
+            theme="github"
+            mode="javascript"
+            defaultValue={ this.state.code }
+            width="100%"
+            height="100%"
+            showPrintMargin={ false }
+            onChange={ code => this.handleChange(code) }
+            editorProps={{ $blockScrolling: true }}
+            setOptions={{ useWorker: false }} />
+        </div>
+        <div className="layout-split-halfpanel">
+          <div className="preview" style={{ height: "100%" }} ref={ this.previewRef }> </div>
+        </div>
+      </div>
+      <div className="timeline">
+        <h1>Timeline</h1>
+        <button onClick={ () => this.saveTimeline() }>Debug</button>
+        <div className="layout-hstack layout-hstack-anchorRight" ref={ this.framesRef }>
+          { timelineFrames }
+        </div>
       </div>
     </div>
   }
